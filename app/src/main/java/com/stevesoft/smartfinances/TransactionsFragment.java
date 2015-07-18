19ac5f22aec08df96b5e63673917810c616a1cb2
Steve <steve.alogaris@outlook.com>
2015-07-18 18:15:37 +0300

    initial commit

---
+++ app/src/main/java/com/stevesoft/smartfinances/TransactionsFragment.java
@@ -0,0 +1,30 @@
+package com.stevesoft.smartfinances;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TransactionsFragment extends Fragment {
+
+
+    public TransactionsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transactions, container, false);
+    }
+
+
+}
